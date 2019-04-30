afd65ff55499c5e878b8f74e7eb5da201b028403
AzureAD\RaquelFlores <raquel.flores@quantion.com>
2019-04-30 21:39:36 +0200

    Add home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import { HomeComponent } from './contracts/home/home.component';
 
 // The order is important. From Most specific to less
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' }
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
